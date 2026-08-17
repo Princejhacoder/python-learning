a11ffe9a291efc2d846d895055eff69803b20c6c
Prince Jha <crimedecodeofficial@gmail.com>
2026-08-17 10:17:34 +0530

    Add Python variables and data types practice

---
+++ 01-basics/variables.py
@@ -0,0 +1,42 @@
+# print("Hello, World!")
+# print("Prince Jha is my name.")
+# print(26+26)
+
+# # Variables
+# name = "Prince Jha" #string variable
+# age = 20 #integer variable
+# price = 100.50 #float variable
+
+# print(name)
+# print(age)
+# print(price)
+
+# # type of variable
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+# Data types
+# age = 23
+# old = False
+# a = None
+# print(type(old))
+# print(type(a))
+
+# Keywords
+# and        else       in        return
+# as         except     is        True
+# assert     finally    lambda    try
+# break      False      nonlocal  with
+# class      for        None      while
+# continue   from       not       yield
+# def        global     or
+# del        if         pass
+# elif       import     raise
+
+
+# Print Sum
+a = 10
+b = 16
+sum = a + b
+print(sum)
